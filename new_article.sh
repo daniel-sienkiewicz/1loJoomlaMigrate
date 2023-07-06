6487a68879812d77dae6d25baed08437e198e5b8
Daniel Sienkiewicz <daniel@sienkiewicz.ovh>
2023-07-06 12:45:47 +0200

    Create new_article.sh

---
+++ new_article.sh
@@ -0,0 +1,4 @@
+curl --location 'https://<URL>/api/index.php/v1/content/articles' \
+--header 'Content-Type: application/json' \
+--header 'X-Joomla-Token: <TOKEN>' \
+--data '{"alias":"my-article11111","state":1,"created":"2023-07-04 07:46:05","articletext":"My text","catid":17,"language":"*","metadesc":"","metakey":"","title":"Here'\''s an article"}'
